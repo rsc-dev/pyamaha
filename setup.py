9b2f22c3bf4765c66bc7bd202de24cf3508b6644
rsc-dev <roscoe@o2.pl>
2017-10-06 16:46:06 +0200

    Version 0.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'pyamaha',
   packages = ['pyamaha'],
-  version = '0.1',
+  version = '0.3',
   description = 'Python implementation of Yamaha Extended Control API Specification.',
   author = 'Radoslaw Matusiak',
   author_email = 'radoslaw.matusiak@gmail.com',
   url = 'https://github.com/rsc-dev/pyamaha',
-  download_url = 'https://github.com/rsc-dev/pyamaha/releases/tag/0.1',
+  download_url = 'https://github.com/rsc-dev/pyamaha/releases/tag/0.3',
   keywords = ['Yamaha', 'API', 'Yamaha Extended Control'],
   classifiers = [
     'Development Status :: 4 - Beta',
